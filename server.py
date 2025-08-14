2cbf76f2049ef0c52113d731ea9517eb621345ea
Wolke56 <junki1699@kakao.com>
2025-08-14 16:03:43 +0900

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def health():
 
 @app.route('/version')
 def version():
-    return {"version": "1.0.0"}, 200
+    return {"version": "yeseo"}, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
